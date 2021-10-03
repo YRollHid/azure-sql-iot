a93ffa9063ceb72229f141967e24e96536746ec8
YRollHid <yans.roll@gmail.com>
2021-10-03 17:02:18 +0200

    Update SetAzureFirewallRule script

--- scripts/SetAzureFirewallRule.ps1
+++ scripts/SetAzureFirewallRule.ps1
@@ -8,5 +8,22 @@ param
   [String] $AzureFirewallR2Name = "AzDOAllowedIPs"
 )
 $agentIP = (New-Object net.webclient).downloadstring("http://checkip.dyndns.com") -replace "[^\d\.]"
-New-AzSqlServerFirewallRule -ResourceGroupName $ResourceGroup -ServerName $ServerName -FirewallRuleName $AzureFirewallR1Name -StartIPAddress $agentIp -EndIPAddress $agentIP
-New-AzSqlServerFirewallRule -ResourceGroupName $ResourceGroup -ServerName $ServerName -FirewallRuleName $AzureFirewallR2Name -StartIPAddress $ipAddress -EndIPAddress $ipAddress
+
+$checkRule1 = Get-AzSqlServerFirewallRule -ResourceGroupName $ResourceGroup -ServerName $ServerName -FirewallRuleName $AzureFirewallR1Name
+
+$checkRule2 = Get-AzSqlServerFirewallRule -ResourceGroupName $ResourceGroup -ServerName $ServerName -FirewallRuleName $AzureFirewallR2Name
+
+if ( $checkRule1 ) {
+    Write-Output "Rule has been added already!"
+}
+else
+{
+    New-AzSqlServerFirewallRule -ResourceGroupName $ResourceGroup -ServerName $ServerName -FirewallRuleName $AzureFirewallR1Name -StartIPAddress $agentIp -EndIPAddress $agentIP
+}
+
+if ( $checkRule2 ) {
+    Write-Output "Rule has been added already!"
+}
+else {
+    New-AzSqlServerFirewallRule -ResourceGroupName $ResourceGroup -ServerName $ServerName -FirewallRuleName $AzureFirewallR2Name -StartIPAddress $ipAddress -EndIPAddress $ipAddress
+}
