c18ea01659c303e40ea70ad6714ba8159e85cc55
YRollHid <yans.roll@gmail.com>
2021-10-03 15:16:33 +0200

    Add SetAzureFirewallRule.ps1 script

---
+++ scripts/SetAzureFirewallRule.ps1
@@ -0,0 +1,12 @@
+[CmdletBinding(DefaultParameterSetName = 'None')]
+param
+(
+  [String] [Parameter(Mandatory = $true)] $ServerName,
+  [String] [Parameter(Mandatory = $true)] $ResourceGroup,
+  [String] [Parameter(Mandatory = $true)] $ipAddress
+  [String] $AzureFirewallR1Name = "CloudAdminAllowedIPs",
+  [String] $AzureFirewallR2Name = "AzDOAllowedIPs"
+)
+$agentIP = (New-Object net.webclient).downloadstring("http://checkip.dyndns.com") -replace "[^\d\.]"
+New-AzSqlServerFirewallRule -ResourceGroupName $ResourceGroup -ServerName $ServerName -FirewallRuleName $AzureFirewallR1Name -StartIPAddress $agentIp -EndIPAddress $agentIP
+New-AzSqlServerFirewallRule -ResourceGroupName $ResourceGroup -ServerName $ServerName -FirewallRuleName $AzureFirewallR2Name -StartIPAddress $ipAddress -EndIPAddress $ipAddress
